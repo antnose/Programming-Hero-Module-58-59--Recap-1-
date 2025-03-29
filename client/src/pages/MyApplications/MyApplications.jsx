a18cb0c05926378282d7e03cd48b4a1123a48e5f
antnose <antnose28@gmail.com>
2025-03-29 18:36:33 +0600

    complete module 58

--- client/src/pages/MyApplications/MyApplications.jsx
+++ client/src/pages/MyApplications/MyApplications.jsx
@@ -61,7 +61,7 @@ const MyApplications = () => {
               </td>
               <td>Purple</td>
               <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                <button className="btn btn-ghost btn-xs">Delete</button>
               </th>
             </tr>
           ))}
